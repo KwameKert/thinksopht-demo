ea3cbe7097e1ae6dec03dc110166473be1ece0a2
mimi <mimsgh2018@gmail.com>
2020-08-26 19:23:31 +0000

    edited the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,15 +8,16 @@ import React from "react";
 const Footer = (props) => {
   return (
     <footer id="footer">
-      <div className="footer-top">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-4 col-md-6 footer-contact">
+      <div class="footer-top">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-3 col-md-6 footer-contact">
               <h3>thinkSopht</h3>
               <p>
                 Mango Street <br />
-                Accra, Ghana
+                Accra
                 <br />
+                Ghana <br />
                 <br />
                 <strong>Phone:</strong> +233 204466965
                 <br />
@@ -25,80 +26,84 @@ const Footer = (props) => {
               </p>
             </div>
 
-            <div className="col-lg-4 col-md-6 footer-links">
+            <div class="col-lg-2 col-md-6 footer-links">
               <h4>Useful Links</h4>
               <ul>
                 <li>
-                  <i className="bx bx-chevron-right"></i> <a href="#">Home</a>
+                  <a href="#">Home</a>
                 </li>
                 <li>
-                  <i className="bx bx-chevron-right"></i>{" "}
                   <a href="#">About us</a>
                 </li>
                 <li>
-                  <i className="bx bx-chevron-right"></i>{" "}
                   <a href="#">Services</a>
                 </li>
                 <li>
-                  <i className="bx bx-chevron-right"></i>
                   <a href="#">Terms of service</a>
                 </li>
                 <li>
-                  <i className="bx bx-chevron-right"></i>
                   <a href="#">Privacy policy</a>
                 </li>
               </ul>
             </div>
 
-            <div className="col-lg-4 col-md-6 footer-links">
+            <div class="col-lg-3 col-md-6 footer-links">
               <h4>Our Services</h4>
               <ul>
                 <li>
-                  <i className="bx bx-chevron-right"></i>{" "}
-                  <a href="#">Web Design</a>
+                  <a href="#">Mobile</a>
                 </li>
                 <li>
-                  <i className="bx bx-chevron-right"></i>
                   <a href="#">Web Development</a>
                 </li>
 
                 <li>
-                  <i className="bx bx-chevron-right"></i>{" "}
                   <a href="#">Digital Marketing</a>
                 </li>
                 <li>
-                  <i className="bx bx-chevron-right"></i>
                   <a href="#">Graphic Design</a>
                 </li>
               </ul>
+            </div>
+
+            <div class="col-lg-4 col-md-6 footer-newsletter">
+              <h4>Join Our Newsletter</h4>
+              <p>
+                Tamen quem nulla quae legam multos aute sint culpa legam noster
+                magna
+              </p>
+              <form action="" method="post">
+                <input type="email" name="email" />
+                <input type="submit" value="Subscribe" />
+              </form>
             </div>
           </div>
         </div>
       </div>
 
-      <div className="container">
-        <div className="copyright-wrap d-md-flex py-4">
-          <div className="mr-md-auto text-center text-md-left">
-            <div className=" mx-auto">
+      <div class="container">
+        <div class="copyright-wrap d-md-flex py-4">
+          <div class="mr-md-auto text-center text-md-left">
+            <div class="copyright">
               &copy;{new Date().getFullYear()} thinkSopht|All right
               reserved|Term of Service|Privacy
             </div>
           </div>
-          <div className="social-links text-center text-md-right pt-3 pt-md-0">
-            <a href="#" className="twitter">
-              <i className="bx bxl-twitter"></i>
+          <div class="social-links text-center text-md-right pt-3 pt-md-0">
+            <a href="#" class="twitter">
+              <i class="bx bxl-twitter"></i>
             </a>
-            <a href="#" className="facebook">
-              <i className="bx bxl-facebook"></i>
+            <a href="#" class="facebook">
+              <i class="bx bxl-facebook"></i>
             </a>
-            <a href="#" className="instagram">
-              <i className="bx bxl-instagram"></i>
+            <a href="#" class="instagram">
+              <i class="bx bxl-instagram"></i>
             </a>
-            <a href="#" className="google-plus">
-              <i className="bx bxl-skype"></i>
+            <a href="#" class="google-plus">
+              <i class="bx bxl-skype"></i>
             </a>
-            <a href="#" className="linkedin">
-              <i className="bx bxl-linkedin"></i>
+            <a href="#" class="linkedin">
+              <i class="bx bxl-linkedin"></i>
             </a>
           </div>
         </div>
